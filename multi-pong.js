7743d4c7afc0466063f16884f1c34ec1ba82a9f9
jcprietov <95312606+jcprietov@users.noreply.github.com>
2021-11-30 16:04:50 -0500

    Update multi-pong.js

--- multi-pong.js
+++ multi-pong.js
@@ -37,4 +37,72 @@ public class Pingpong extends JPanel {
         racquet.move();
     }
 
+ @Override 
+
+    public void paint(Graphics g) { 
+
+        super.paint(g); 
+
+        Graphics2D g2d = (Graphics2D) g; 
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+
+                RenderingHints.VALUE_ANTIALIAS_ON); 
+
+        ball.paint(g2d); 
+
+        racquet.paint(g2d); 
+
+        g2d.setColor(Color.GRAY); 
+
+        g2d.setFont(new Font("Verdana", Font.BOLD, 30)); 
+
+        g2d.drawString(String.valueOf(getScore()), 10, 30); 
+
+    } 
+
+  
+
+    public void gameOver() { 
+
+        JOptionPane.showMessageDialog(this, "Su puntaje fue: " + getScore(), 
+
+                "Game Over", JOptionPane.YES_NO_OPTION); 
+
+        System.exit(ABORT); 
+
+    } 
+
+  
+
+    public static void main(String[] args) throws InterruptedException { 
+
+        JFrame frame = new JFrame("Ping Pong"); 
+
+        Pingpong juego = new Pingpong(); 
+
+        frame.add(juego); 
+
+        frame.setSize(400, 400); 
+
+        frame.setVisible(true); 
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+  
+
+        while (true) { 
+
+            juego.move(); 
+
+            juego.repaint(); 
+
+            Thread.sleep(10); 
+
+        } 
+
+    } 
+
+} 
+
     
